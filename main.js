92b2c8ed0c1451ea24fce004329f6ba95a00eb65
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-06 12:54:00 -0500

    calculation push

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 const keyNum = document.querySelectorAll('.number');
 const operNum = document.querySelectorAll('.operator');
-const eqlOper = document.querySelectorAll('.equal-sign');
-const clr = document.querySelectorAll('.clear');
-
+const eqlOper = document.querySelector('.equal-sign');
+const clr = document.querySelector('.clear');
+const calcScreen = document.querySelector('.calculator-screen');
 
 let calculation = [];
 
-keyNum.forEach(function(keyNum){
+keyNum.forEach(function (keyNum) {
     keyNum.addEventListener('click', function pushNumber(event) {
-        alert(keyNum.textContent);
-    }) 
+        alert(keyNum.value);
+        calculation.push(keyNum.value);
+        console.log(calculation);
+    })
 });
 
-operNum.forEach(function(operNum){
-    operNum.addEventListener('click', function pushNumber(event) {
-        alert(operNum.textContent);
-    }) 
+operNum.forEach(function (operNum) {
+    operNum.addEventListener('click', function pushOperator(event) {
+        alert(operNum.value);
+        calculation.push(operNum.value);
+        console.log(calculation);
+    })
 });
 
-eqlOper.forEach(function(eqlOper){
-    eqlOper.addEventListener('click', function pushNumber(event) {
-        alert(eqlOper.textContent);
-    }) 
-});
+eqlOper.addEventListener('click', function calculate(event) {
+    alert(eqlOper.value);
+})
 
-clr.forEach(function(clr){
-    clr.addEventListener('click', function pushNumber(event) {
-        alert(clr.textContent);
-    }) 
-});
+
+clr.addEventListener('click', function clear(event) {
+    alert(clr.value);
+})
+
+
+
