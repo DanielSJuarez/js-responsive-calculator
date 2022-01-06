d96ec9c2d202c7d33a22cb93ee38cca3e39f74f2
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-06 12:07:40 -0500

    key and operator alert

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const keyNum = document.querySelectorAll('.number');
+const operNum = document.querySelectorAll('.operator');
+
+keyNum.forEach(function(keyNum){
+    keyNum.addEventListener('click', function pushNumber(event) {
+        alert(keyNum.textContent);
+    }) 
+});
+
+operNum.forEach(function(operNum){
+    operNum.addEventListener('click', function pushNumber(event) {
+        alert(operNum.textContent);
+    }) 
+});
