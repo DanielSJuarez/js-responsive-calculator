ae1afe9e2de2d9d796abd8e089bd942d934ba35b
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-07 12:35:37 -0500

    calculation engine update

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const eqlOper = document.querySelector('.equal-sign');
 const clr = document.querySelector('.clear');
 const calcScreen = document.querySelector('.calculator-screen');
 
-const compareNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const compareOper = ['+', '-', '*', '/'];
+//const compareNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//const compareOper = ['+', '-', '*', '/'];
 let calculation = [];
 let oldNumber =[];
-let currentOper = [];
+let currentOper = '';
 let currentNumber = [];
-let calcValue = '';
+//let calcValue = '';
 calcScreen.disable = true;
 
 keyNum.forEach(function (keyNum) {
@@ -27,7 +27,7 @@ operNum.forEach(function (operNum) {
     operNum.addEventListener('click', function pushOperator(event) {
         //alert(operNum.value);
         calculation.push(operNum.value);
-        currentOper.push(operNum.value);
+        currentOper = operNum.value;
         oldNumber = currentNumber;
         currentNumber = [];
         calcScreen.value = calculation.join('');
@@ -41,21 +41,28 @@ eqlOper.addEventListener('click', function calculate(event) {
     //alert(eqlOper.value);
     //console.log(calculation.join(''));
     // for (let i = 0; i < calculation.length; i++) {
-        //console.log(currentOper);
+        //let strOper = currentOper.toString();
+        console.log(currentOper);
         //console.log(currentNumber)
         let oldNumStr = parseFloat(oldNumber.toString());
         let newNumStr = parseFloat(currentNumber.toString());
-        //console.log(typeof newNumStr)
+        // console.log(oldNumStr)
+        // console.log(newNumStr)
+        //console.log(oldNumStr - newNumStr);
     
-        if (currentOper = ['+']){
+        if (currentOper === '+'){
+            console.log('add');
             calcScreen.value = oldNumStr + newNumStr;   
-        } else if (currentOper = ['-']){
+        } else if (currentOper === '-'){
+            console.log('sub');
             calcScreen.value = oldNumStr - newNumStr;  
-        } else if (currentOper = ['*']){
-            calcScreen.value = oldNumStr * newNumStr;   
-        } else if (currentOper = ['/']){
+        } else if (currentOper === '*'){
+            console.log('mul');
+            calcScreen.value === oldNumStr * newNumStr;   
+        } else if (currentOper = '/'){
+            console.log('div');
             calcScreen.value = oldNumStr / newNumStr;  
-        } else {
+        }  else {
             calcScreen.value = "Error";
         }
 })
@@ -65,7 +72,7 @@ clr.addEventListener('click', function clear(event) {
     //alert(clr.value);'
     calculation = [];
     oldNumber = [];
-    currentOper = [];
+    currentOper = '';
     currentNumber = [];
     calcValue = '';
     calcScreen.value = 0
