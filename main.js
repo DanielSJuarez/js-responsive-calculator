4a81add3ee02856f175aa9e33dde1cc331666eaf
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-07 12:43:37 -0500

    final required commit

--- main.js
+++ main.js
@@ -29,33 +29,23 @@ operNum.forEach(function (operNum) {
     })
 });
 
-
-
 eqlOper.addEventListener('click', function calculate(event) {
-    // for (let i = 0; i < calculation.length; i++) {
-    //let strOper = currentOper.toString();
-    console.log(currentOper);
 
     let oldNumStr = parseFloat(oldNumber.join('').toString());
     let newNumStr = parseFloat(currentNumber.join('').toString());
 
     if (currentOper === '+') {
-        console.log('add');
         calcScreen.value = oldNumStr + newNumStr;
     } else if (currentOper === '-') {
-        console.log('sub');
         calcScreen.value = oldNumStr - newNumStr;
     } else if (currentOper === '*') {
-        console.log('mul');
         calcScreen.value = oldNumStr * newNumStr;
     } else if (currentOper = '/') {
-        console.log('div');
         calcScreen.value = oldNumStr / newNumStr;
     } else {
         calcScreen.value = "Error";
     }
 })
-
 
 clr.addEventListener('click', function clear(event) {
     calculation = [];
